82cc2b78118f38409622b752f174a429337e20c0
prashanna ghimire <ghimireprashanna11@gmail.com>
2025-04-17 10:22:49 +0545

    animation_inserted

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { assets, testimonialsData } from '../assets/assets'
+import { delay, motion } from "motion/react";
 
 
 const Testimonials = () => {
   return (
-    <div className='flex flex-col items-center justify-center my-20 py-12 '>
+    <motion.div 
+    initial={{opacity:0.2, y:100}}
+      transition={{duration:1}}
+      whileInView={{opacity:1, y:0}}
+      viewport={{once:true}}
+    className='flex flex-col items-center justify-center my-20 py-12 '>
 
         <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Customer testimonials</h1>
         <p className='text-gray-500 mb-12'>What our users are saying</p>
@@ -26,7 +32,7 @@ const Testimonials = () => {
                 </div>
             ))}
         </div>
-    </div>
+    </motion.div>
   )
 }
 
